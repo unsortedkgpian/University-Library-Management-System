ffbe1b4aaf7f46c312a6f67308ac4009fb450d97
unsortedkgpian <theadityakumar2810@gmail.com>
2025-01-29 20:46:09 +0530

    Home Data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
-import { Button } from "../../components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
 	return (
 		<>
-			<BookOverview />
-			<BookList />
+			<BookOverview {...sampleBooks[0]} />
+			<BookList
+				title="Latest Book"
+				books={sampleBooks}
+				containerClassName="mt-28"
+			/>
 		</>
 	);
 }
