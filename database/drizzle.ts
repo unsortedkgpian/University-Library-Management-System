b6a295b1dc0ab4d033403faa296320051589bd81
unsortedkgpian <theadityakumar2810@gmail.com>
2025-02-07 20:37:05 +0530

    user onboding overflow

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -4,7 +4,7 @@ import config from "@/lib/config";
 
 // config({ path: ".env" }); // or .env.local
 console.log("database url", process.env.DATABASE_URL!);
-const db_url = process.env.NEXT_PUBLIC_DATABASE_URL;
+const db_url = process.env.NEXT_PUBLIC_DATABASE_URL!;
 const sql = neon(db_url!);
 console.log("sql", sql);
 export const db = drizzle({ client: sql });
